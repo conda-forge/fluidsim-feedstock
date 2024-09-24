21609c1d354d9e41bd76344ff40e45b0bf1f425b
paugier <pierre.augier@univ-grenoble-alpes.fr>
2024-09-24 15:40:31 +0200

    Fix ARM OSX builds

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+set -xeuo pipefail
 
-$PYTHON -m pip install --no-deps --ignore-installed -vv .
+# taken from https://github.com/conda-forge/scikit-image-feedstock/blob/main/recipe/build.sh
+
+mkdir builddir
+
+# need to run meson first for cross-compilation case
+${PYTHON} $(which meson) setup ${MESON_ARGS} \
+    builddir || (cat builddir/meson-logs/meson-log.txt && exit 1)
+
+${PYTHON} -m build --wheel --no-isolation --skip-dependency-check -Cbuilddir=builddir
+${PYTHON} -m pip install dist/fluidsim*.whl
